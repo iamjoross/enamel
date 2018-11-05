05590af1d82be8bf24431aac9571fcce5153a5bf
joross <joross.barredo@treatspace.com>
2018-11-05 10:43:46 +0800

    step3

---
+++ client/src/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Signup from './views/Signup.vue'
+import Login from './views/Login.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: { title: 'enamel' }
+    },
+    {
+      path: '/signup/:id',
+      name: 'signup',
+      component: Signup,
+      meta: { title: 'Signup - enamel' }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { title: 'Login - enamel' }
+     }
+  ]
+})
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
+})
+export default router
